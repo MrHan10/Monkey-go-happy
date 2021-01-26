77edaa4950b4b39fb73809679e037b32328d906a
MrHan10 <72027818+MrHan10@users.noreply.github.com>
2021-01-26 20:00:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,103 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  
+ createCanvas(600, 600);
+
+ 
+  monkey = createSprite(50,160,20,50);
+  monkey.addAnimation("running", monkey_running);
+  
+  
+  monkey.scale = 0.1;
+  
+  ground = createSprite(300,400,600,20);
+  
+  FoodGroup = new Group();
+  obstacleGroup = new Group();
+  
+}
+
+
+function draw() {
+  background("white");
+  ground.velocityX = -3
+  
+   if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+    
+    //jump when the space key is pressed
+    if(keyDown("space")&& monkey.y >= 100) {
+        monkey.velocityY = -12;
+      }
+    
+    //add gravity
+    monkey.velocityY = monkey.velocityY + 0.8
+    monkey.collide(ground);
+  
+ drawSprites();
+  
+  spawnObstacles();
+  spawnFood();
+  
+}
+
+function spawnObstacles() {
+  if(frameCount % 300 === 0) {
+    obstacle = createSprite(800,320,10,40);
+    obstacle.velocityX = -6;
+    
+    //add image to the obstacle 
+    obstacle.addImage(obstaceImage);
+    obstacle.scale=0.15;
+    
+    //lifetime to the obstacle     
+    obstacle.lifetime = 300;
+    
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+  }
+}
+
+function spawnFood() {
+  //write code here to spawn the Food
+  if (frameCount % 80 === 0) {
+    banana = createSprite(600,250,40,10);
+    banana.y = random(120,200);    
+    banana.velocityX = -5;
+    
+     //assign lifetime to the variable
+    banana.lifetime = 300;
+    monkey.depth = banana.depth + 1;
+    
+    //add image of banana
+     banana.addImage(bananaImage);
+     banana.scale=0.05;
+    
+    //add each banana to the group
+    FoodGroup.add(banana);
+  }
+}
+
+
+
+
+
+
